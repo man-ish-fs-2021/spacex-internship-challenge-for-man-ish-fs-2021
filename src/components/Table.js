1e0be3bb49383a741c24a3d279e0bdce9f0ae477
pubehunter <manishchandra58@gmail.com>
2021-06-09 20:18:26 +0530

    Basic pagination setup

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,10 @@
 import React, { useMemo } from "react";
 import { useEffect } from "react";
+import { Pagination } from "./index";
 import { fetchLaunch } from "../actions/launch";
 import { useDispatch, useSelector } from "react-redux";
 import { COLUMNS } from "./columns";
-import { useTable } from "react-table";
+import { useTable, usePagination } from "react-table";
 export const Table = () => {
   const launch = useSelector((state) => state.launch);
   // console.log("Launch", launch);
@@ -14,51 +15,56 @@ export const Table = () => {
   //   dispatch(fetchLaunch());
   const columns = useMemo(() => COLUMNS, []);
   const data = launch;
-  const tableInstance = useTable({
-    columns,
-    data,
-  });
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+    },
+    usePagination
+  );
+  const { getTableProps, getTableBodyProps, headerGroups, page, prepareRow } =
     tableInstance;
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps({
-                  style: { minWidth: column.minWidth },
-                })}
-              >
-                {column.render("Header")}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()} className="tr-body">
-              {row.cells.map((cell) => {
-                return (
-                  <td
-                    {...cell.getCellProps({
-                      style: { minWidth: cell.column.minWidth },
-                    })}
-                  >
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps({
+                    style: { minWidth: column.minWidth },
+                  })}
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} className="tr-body">
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      {...cell.getCellProps({
+                        style: { minWidth: cell.column.minWidth },
+                      })}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination {...tableInstance} />
+    </>
   );
 };
 
